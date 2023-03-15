c473f7b7ece482d9887e836ef0bd6dca38cb7fa2
Antonio Muñoz <antoniogmc@gmail.com>
2023-03-15 01:43:45 +0100

    calculate converters in constructor

--- src/main/java/pq/internal/JsonGroupConverter.java
+++ src/main/java/pq/internal/JsonGroupConverter.java
@@ -1,52 +1,48 @@
 package pq.internal;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import static java.util.Objects.requireNonNull;
+
 import java.util.function.Consumer;
 
 import org.apache.parquet.io.api.Converter;
 import org.apache.parquet.io.api.GroupConverter;
 import org.apache.parquet.schema.GroupType;
-import org.apache.parquet.schema.Type;
 
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 
 public final class JsonGroupConverter extends GroupConverter {
   
-  private final GroupType schema;
   private final Consumer<JsonValue> consumer;
-  private final Map<String, Converter> converters = new HashMap<>();
+  private final Converter[] converters;
 
   private JsonObject value;
   
   public JsonGroupConverter(GroupType schema, Consumer<JsonValue> consumer) {
-    this.schema = Objects.requireNonNull(schema);
-    this.consumer = consumer;
+    this.consumer = requireNonNull(consumer);
+    this.converters = new Converter[schema.getFieldCount()];
+    for (var fieldType : schema.getFields()) {
+      String fieldName = fieldType.getName();
+      if (fieldType.isPrimitive()) {
+        var converter = switch (fieldType.asPrimitiveType().getPrimitiveTypeName()) {
+          case INT32 -> new JsonPrimitiveConverterFactory.IntegerConverter(v -> value.add(fieldName, v));
+          case INT64 -> new JsonPrimitiveConverterFactory.LongConverter(v -> value.add(fieldName, v));
+          case FLOAT -> new JsonPrimitiveConverterFactory.FloatConverter(v -> value.add(fieldName, v));
+          case DOUBLE -> new JsonPrimitiveConverterFactory.DoubleConverter(v -> value.add(fieldName, v));
+          case BOOLEAN -> new JsonPrimitiveConverterFactory.BooleanConverter(v -> value.add(fieldName, v));
+          case BINARY -> new JsonPrimitiveConverterFactory.StringConverter(v -> value.add(fieldName, v));
+          default -> throw new UnsupportedOperationException("not supported type: " + fieldType);
+        };
+        converters[schema.getFieldIndex(fieldName)] = converter;
+      } else {
+        converters[schema.getFieldIndex(fieldName)] = new JsonGroupConverter(fieldType.asGroupType(), v -> value.add(fieldName, v));
+      }
+    }
   }
 
   @Override
   public Converter getConverter(int fieldIndex) {
-    Type fieldType = getFieldType(fieldIndex);
-    String fieldName = fieldType.getName();
-    if (fieldType.isPrimitive()) {
-      var converter = switch (fieldType.asPrimitiveType().getPrimitiveTypeName()) {
-        case INT32 -> new JsonPrimitiveConverterFactory.IntegerConverter(v -> value.add(fieldName, v));
-        case INT64 -> new JsonPrimitiveConverterFactory.LongConverter(v -> value.add(fieldName, v));
-        case FLOAT -> new JsonPrimitiveConverterFactory.FloatConverter(v -> value.add(fieldName, v));
-        case DOUBLE -> new JsonPrimitiveConverterFactory.DoubleConverter(v -> value.add(fieldName, v));
-        case BOOLEAN -> new JsonPrimitiveConverterFactory.BooleanConverter(v -> value.add(fieldName, v));
-        case BINARY -> new JsonPrimitiveConverterFactory.StringConverter(v -> value.add(fieldName, v));
-        default -> throw new UnsupportedOperationException("not supported type: " + fieldType);
-      };
-      converters.put(fieldName, converter);
-      return converter;  
-    } else {
-      var converter = new JsonGroupConverter(fieldType.asGroupType(), v -> value.add(fieldName, v));
-      converters.put(fieldName, converter);
-      return converter;  
-    }
+    return converters[fieldIndex];
   }
 
   @Override
@@ -58,9 +54,4 @@ public final class JsonGroupConverter extends GroupConverter {
   public void end() {
     consumer.accept(value);
   }
-  
-  private Type getFieldType(int fieldIndex) {
-    return schema.getFields().get(fieldIndex);
-  }
-
 }
