8fb88dafeb05997067e5ee0b05f2948f9de4f3d0
Antonio Muñoz <antoniogmc@gmail.com>
2026-02-28 14:41:10 +0100

    Update copyright year from 2025 to 2026 in all relevant files

--- src/main/java/pq/internal/JsonParquetReader.java
+++ src/main/java/pq/internal/JsonParquetReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2025, Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
+ * Copyright (c) 2023-2026, Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
  * Distributed under the terms of the MIT License
  */
 package pq.internal;
